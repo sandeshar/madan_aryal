fe472c5699b9dc069228769c15c741ee0b29d684
aryal-sandesh <sandesharyal.135@gmail.com>
2025-09-29 19:10:26 +0545

    Fix indentation in CounterClient component for improved readability

--- src/components/CounterClient.tsx
+++ src/components/CounterClient.tsx
@@ -3,12 +3,12 @@ import React from "react";
 import CountUp from "react-countup";
 
 type Props = {
-  end: number | string;
-  suffix?: string;
+    end: number | string;
+    suffix?: string;
 };
 
 const CounterClient: React.FC<Props> = ({ end, suffix }) => {
-  return <CountUp end={Number(end)} suffix={suffix} duration={2} />;
+    return <CountUp end={Number(end)} suffix={suffix} duration={2} />;
 };
 
 export default CounterClient;
